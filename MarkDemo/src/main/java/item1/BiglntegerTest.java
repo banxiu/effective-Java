d4c7b002aba6cad5c904f7f118cdfdc19b4cb33d
wangly <stubborn999@163.com>
2021-01-27 17:40:23 +0800

    [add] 十进制转二进制

--- MarkDemo/src/main/java/item1/BiglntegerTest.java
+++ MarkDemo/src/main/java/item1/BiglntegerTest.java
@@ -1,4 +1,4 @@
-package Item1;
+package item1;
 
 import java.math.BigInteger;
 import java.util.Scanner;
@@ -15,21 +15,49 @@ import java.util.Scanner;
  * • Biglnteger divide(Biglnteger other) 商
  * • Biglnteger mod(Biglnteger other) 余
  * 返冋这个大整数和另一个大整数 other•的和、 差、 积、 商以及余数。
+ * compareTo: 正常其它数据类型规则是
+ * CompareTo(xx x,xx y)
+ * x<y : -1
+ * x=y : 0
+ * x>y : 1
+ * BigInteger :
+ *
+ *
+ * public int compareTo(BigInteger val) {
+ *         if (this.signum == val.signum) {
+ *             switch(this.signum) {
+ *             case -1:
+ *                 return val.compareMagnitude(this);
+ *             case 1:
+ *                 return this.compareMagnitude(val);
+ *             default:
+ *                 return 0;
+ *             }
+ *         } else {
+ *             return this.signum > val.signum ? 1 : -1;
+ *         }
+ *     }
+ *
+ *
  */
 public class BiglntegerTest {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        /*Scanner in = new Scanner(System.in);
         System.out.println();
         System.out.print("How many numbers do you need to draw? ");
         int k = in.nextInt();
 
         System.out.print("What is the highest number you can draw? ");
         int n = in.nextInt();
-        /* compute binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k)*/
+        *//* compute binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k)*//*
         BigInteger lotteryOdds = BigInteger.valueOf(1);
         for (int i = 1; i < k; i++) {
             lotteryOdds = lotteryOdds.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
         }
-        System.out.println("Your odds are 1 in " + lotteryOdds +". Good luck!");
+        System.out.println("Your odds are 1 in " + lotteryOdds +". Good luck!");*/
+        System.out.println((long)16 & 4);
+        //十进制转二进制 控制台显示
+        System.out.println(Integer.toBinaryString(4));
+
     }
 }
--- MarkDemo/src/main/java/item1/TestDemo.java
+++ MarkDemo/src/main/java/item1/TestDemo.java
@@ -1,9 +1,6 @@
-package Item1;
+package item1;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
 
 /**
