f5cb5f04188131f39a1c2c62417d64cd686054dc
wangly <stubborn999@163.com>
2021-02-01 10:11:59 +0800

    [add] 二维数组利率问题

--- MarkDemo/src/main/java/item1/Compoundlnterest.java
+++ MarkDemo/src/main/java/item1/Compoundlnterest.java
@@ -22,8 +22,28 @@ public class Compoundlnterest {
             balances[0][i] = 10000;
         }
         // compute interest for future years
-        for (int i = 0; i < balances.length; i++) {
+        for (int i = 1; i < balances.length; i++) {
+            for (int j = 0; j < balances[i].length; j++) {
+                // get last year ' s balances from previous row
+                double oldBalance = balances[i - 1][j];
+                // compute interest
+                double interest = oldBalance * intereatRate[j];
+                // compute this year' s balances
+                balances[i][j] = oldBalance + interest;
 
+            }
+        }
+        // print one row of interest rates
+        for (int i = 0; i < intereatRate.length; i++) {
+            System.out .printf("%9.0f%%", 100 * intereatRate[i]);
+        }
+        System.out.println();
+        // print balance table
+        for (double[] doubles : balances) {
+            for (double aDouble : doubles) {
+                System.out.printf("%10.2f",aDouble);
+            }
+            System.out.println();
         }
     }
 
